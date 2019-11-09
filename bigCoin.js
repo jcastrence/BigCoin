c0ba44a98e76e95bcabd5d3bf39c454489120dd6
Julian <juliancastrence@gmail.com>
2019-11-08 19:39:11 -0500

    Transaction Class Encapsulated, Added Balance Function

--- bigCoin.js
+++ bigCoin.js
@@ -12,6 +12,7 @@ let getDateTime = () => {
 // Constants used to generate the genesis block. This is the hash and timestamp of Bitcoin's genesis block.
 const GENESISHASH = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
 const GENESISTIMESTAMP = '2009-1-3 13:15:05';
+const GENESISREWARD = 50;
 
 // The ledger keeps track of all BigCoin transactions that have ever happened.
 class Ledger {
@@ -20,11 +21,12 @@ class Ledger {
         this.blockChain = [this.createGenesisBlock()];
         this.difficulty = 1;
         this.pendingTransactions = [];
-        this.reward = 50;
+        this.coinbase = 21000000;
+        this.currMiningReward = 50;
     }
 
     createGenesisBlock() {
-        let genesisBlock = new Block(GENESISTIMESTAMP, [], 0, 0);
+        let genesisBlock = new Block(GENESISTIMESTAMP, [], GENESISREWARD, 0, 0);
         genesisBlock.hash = GENESISHASH;
         return genesisBlock;
     }
@@ -33,25 +35,48 @@ class Ledger {
         return this.blockChain[this.blockChain.length - 1];
     }
 
-    addBlock(minerAddress) {
+    mineBlock(minerAddress) {
+        // Create a transaction where the miner will get rewarded from the coinbase if the block is
+        // successfully mined.
+        let minerRewards = new Transaction("coinbase", minerAddress, this.currMiningReward, true);
         this.blockChain.push(
-            this.mineBlock(
-                new Block(getDateTime(), this.pendingTransactions, this.difficulty, this.getLastBlock().getHash())
+            this.mineBlockHelper(
+                new Block(
+                    getDateTime(), [minerRewards, ...this.pendingTransactions], this.currMiningReward,
+                    this.difficulty, this.getLastBlock().getHash()
+                )
             )
         );
+        // Clear the list of pending transactions.
         this.pendingTransactions = [];
-        this.pendingTransactions.push(new Transaction('origin', minerAddress, this.reward));
+        this.coinbase -= this.currMiningReward;
     }
 
-    mineBlock(unminedBlock) {
+    mineBlockHelper(unminedBlock) {
         while(unminedBlock.getHash().substring(0, this.difficulty) !== Array(this.difficulty + 1).join('0')) {
             unminedBlock.recalculateHash();
         }
         return unminedBlock;
     }
 
-    addTransactions(pendingTransactions) {
-        this.pendingTransactions.push(...pendingTransactions);
+    requestTransaction(sendAddress, receiveAddress, amount) {
+        this.pendingTransactions.push(new Transaction(sendAddress, receiveAddress, amount));
+    }
+
+    getBalance(address) {
+        let balance = 0;
+        let i = 0;
+        this.blockChain.forEach((block) => {
+            block.transactions.forEach((transaction) => {
+                if (address === transaction.receiveAddress) {
+                    balance += transaction.amount;
+                }
+                else if (address === transaction.sendAddress) {
+                    balance -= transaction.amount;
+                }
+            });
+        });
+        return balance;
     }
 
 }
@@ -59,9 +84,10 @@ class Ledger {
 // Each Block object is composed of a header and a list of transactions.
 class Block {
     
-    constructor(timestamp, transactions, difficulty, previousHash) {
+    constructor(timestamp, transactions, reward, difficulty, previousHash) {
         this.timestamp = timestamp;
         this.transactions = transactions;
+        this.reward = reward;
         this.difficulty = difficulty;
         this.previousHash = previousHash;
         this.nonce = 0;
@@ -89,12 +115,12 @@ class Block {
 
 // Transaction class is used to document transactions.
 class Transaction {
-    constructor(sendAddress, receiveAddress, amount) {
+    constructor(sendAddress, receiveAddress, amount, fromCoinbase = false) {
         this.sendAddress = sendAddress;
         this.receiveAddress = receiveAddress;
         this.amount = amount;
+        this.fromCoinbase = fromCoinbase;
     }
 }
 
 module.exports.Ledger = Ledger;
-module.exports.Transaction = Transaction;
--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const Ledger = require('./bigCoin.js').Ledger;
-const Transaction = require('./bigCoin.js').Transaction;
 
 const publicLedger = new Ledger();
-t1 = new Transaction('Alice', 'Bob', 5);
-t2 = new Transaction('Eve', 'Charles', 7);
-publicLedger.addTransactions([t1, t2]);
-publicLedger.addBlock('MikeMiner');
-t3 = new Transaction('Bob', 'Charles', 10);
-publicLedger.addTransactions([t3]);
-publicLedger.addBlock('MikeMiner');
-console.log(publicLedger.blockChain);
+// MikeMiner mines the first block, and is rewarded 50 BigCoins.
+publicLedger.mineBlock('MikeMiner');
+// MikeMiner sends 5 BigCoins to Alice and 7 BigCoins to Bob.
+// These transactions are broadcast to the BigCoin ledger.
+publicLedger.requestTransaction('MikeMiner', 'Alice', 10);
+publicLedger.requestTransaction('MikeMiner', 'Bob', 10);
+// MikeMiner mines the second block.
+publicLedger.mineBlock('MikeMiner');
+// Bob sends 3 BigCoins to Charles.
+// This transaction is broadcast to the BigCoin ledger.
+publicLedger.requestTransaction('Bob', 'Charles', 5);
+// Bob mines the third Block.
+publicLedger.mineBlock('Bob');
+
+
+// Let's look at the blockchain so far...
+console.log("Genesis Block:\n");
+console.log(publicLedger.blockChain[0]);
+console.log("\nBlock 1:\n");
 console.log(publicLedger.blockChain[1]);
+console.log("\nBlock 2:\n");
 console.log(publicLedger.blockChain[2]);
+console.log("\nBlock 3:\n");
+console.log(publicLedger.blockChain[3]);
+console.log("\nBalances:");
+console.log(`MikeMiner: ${publicLedger.getBalance('MikeMiner')}`);
+console.log(`Alice: ${publicLedger.getBalance('Alice')}`);
+console.log(`Bob: ${publicLedger.getBalance('Bob')}`);
+console.log(`Charles: ${publicLedger.getBalance('Charles')}`);
